2c1e4f3b6deb83b0c4fbdde2bccb39ee1f7f99c6
Dr_Dee <digitalis13gmx.de>
2020-12-28 13:05:48 +0100

    skinmanager weitergemacht

--- app/assets/js/scripts/skins.js
+++ app/assets/js/scripts/skins.js
@@ -1,19 +1,24 @@
 const logger = LoggerUtil('%c[SkinManager]', 'color: #110668; font-weight: bold')
 
+const SkinManager = document.getElementById('skin-manager')
+
 let skinRender = new SkinRender({
     autoResize: true,
     render: {
         taa: true
-    }
-}, document.getElementById('skin-manager'))
-skinRender.render(ConfigManager.getSelectedAccount().displayName)
-const startTime = Date.now();
-document.getElementById('skin-manager').addEventListener('skinRender', function (e) {
-    if (animate) {
+    },
+    pauseHidden: false
+}, SkinManager)
+skinRender.render(ConfigManager.getSelectedAccount().displayName, e => console.log(e))
+const startTime = Date.now()
+/* SkinManager.addEventListener('skinRender', function (e) {
+    if (true) {
         let t = (Date.now() - startTime) / 1000
         e.detail.playerModel.children[2].rotation.x = Math.sin(t * 5) / 2
         e.detail.playerModel.children[3].rotation.x = -Math.sin(t * 5) / 2
         e.detail.playerModel.children[4].rotation.x = Math.sin(t * 5) / 2
         e.detail.playerModel.children[5].rotation.x = -Math.sin(t * 5) / 2
     }
-})
+}) */
+
+document.getElementById('skinBtn').addEventListener('click', e => SkinManager.style.display = 'block')
