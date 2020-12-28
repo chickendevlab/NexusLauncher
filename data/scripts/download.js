172918620e94a81964b87eb14a51ab68c338e253
Dr_Dee <digitalis13gmx.de>
2020-12-28 09:29:48 +0100

    script gekürzt

--- data/scripts/download.js
+++ data/scripts/download.js
@@ -1,6 +1,4 @@
 const id = 'id'
-
 fetch('https://ChickenDevLab.github.io/NexusLauncher/data/installer.json').then(response => response.json()).then(data => {
-    console.log(data)
     document.getElementById(id).href = data.latest
 })
